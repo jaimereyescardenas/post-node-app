531e962e2dec54b0887d2df9cde39c294a786cf8
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-02-14 20:54:12 -0400

    App actualizada para recibir parámetros por JSON

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const postRouter = require('./routes/post-routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, access-control-allow-origin');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 
 app.use(postRouter);
 
-app.listen(8080);
+app.listen(8080, () => console.log('Server started!'));
