19fd379b696ec4cb60ef4450018a0d05f1286af1
Jaime Reyes Cárdenas <jaimefabianreyescardenas@gmail.com>
2021-02-15 23:34:31 -0400

    Cambio en manejo de cors

--- app.js
+++ app.js
@@ -10,14 +10,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, access-control-allow-origin');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//     next();
-// });
-
 app.use(postRouter);
 
 app.listen(8080, () => console.log('Server started!'));
